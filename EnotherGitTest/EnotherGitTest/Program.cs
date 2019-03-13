e17123e582a41b0564a377097eece601404c1f2c
Lanseris <grantplazma@mail.ru>
2019-03-13 14:42:09 +0300

    first commit

---
+++ EnotherGitTest/EnotherGitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnotherGitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
