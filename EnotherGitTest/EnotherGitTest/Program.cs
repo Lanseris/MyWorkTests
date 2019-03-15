2e6d66cb408ad0758fa7806e2c90c3ce96bd4ce2
Lanseris <grantplazma@mail.ru>
2019-03-15 09:54:47 +0300

    create programm main menu

--- EnotherGitTest/EnotherGitTest/Program.cs
+++ EnotherGitTest/EnotherGitTest/Program.cs
@@ -7,13 +7,27 @@ namespace EnotherGitTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите номер необходимой операции");
 
-            #region AsyncMeethodTests
-            AsyncTestsClass.ReadWriteAsync();
-            Console.WriteLine("Некоторая работа");
+            Console.WriteLine("1 - проверка асинхронных методов");
 
-            AsyncTestsClass.FactorialAsync();
-            #endregion
+            Console.Write("Номер:");
+
+            if(Int32.TryParse(Console.ReadLine(),out int operationNumber))
+            {
+                switch (operationNumber)
+                {
+                    case 1:
+                        #region AsyncMeethodTests
+                        AsyncTestsClass.ReadWriteAsync();
+                        Console.WriteLine("Некоторая работа");
+                        AsyncTestsClass.FactorialAsync();
+                        #endregion
+                        break;
+                    default:
+                        break;
+                }
+            }
 
             Console.Read();
         }
