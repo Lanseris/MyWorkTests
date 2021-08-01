6cd7b35ed4d22fdcb8fcad88221292bfebdc784e
Vasiliy <grantplazma@mail.ru>
2021-08-01 17:46:16 +0300

    Шпора EventHandler-ов

---
+++ EnotherGitTest/EnotherGitTest/EventHandlers/ClassVsDelegateAndEH.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnotherGitTest.EventHandlers
+{
+    public class ClassVsDelegateAndEH
+    {
+        public delegate void SomeDelegate();
+
+        public event SomeDelegate SomeDelegateEH;
+
+        public void Print()
+        {
+            Console.WriteLine("Текст из метода, который был добавлен в event (сюда SomeDelegateEH) из вне:");
+            //оповещаем всех, кто подписался
+            //(вызываем методы, которые туда добавлены)
+            SomeDelegateEH?.Invoke();
+        }
+    }
+}
---
+++ EnotherGitTest/EnotherGitTest/EventHandlers/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnotherGitTest.EventHandlers
+{
+    //класс, которому важно знать сколько раз вызывался метод Print 
+    public class Counter
+    {
+        int count = 0;
+
+        public Counter(ClassVsDelegateAndEH classVsDelegateAndEH)
+        {
+            //подписка на event в классе, количество вызовов метода которого нам важно
+            //(добавили в event того класса метод из этого класса)
+            classVsDelegateAndEH.SomeDelegateEH += CountUpdate;
+        }
+
+        public void CountUpdate()
+        {
+            count++;
+            Console.WriteLine(count);
+        }
+    }
+}
